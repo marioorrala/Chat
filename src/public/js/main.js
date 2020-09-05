66214787374e950fb575f1af522424509222d733
Mario Orrala <marioorrala@gmail.com>
2020-09-04 22:16:09 -0500

    usuarios en linea

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,62 +1,57 @@
 $(function () {
 
-    const socket = io();
+  const socket = io();
 
-    //obteniendo los elementos del DOM desde la interfaz
-    const $messageForm = $('#message-form');
-    const $messageBox = $('#message');
-    const $chat = $('#chat');
+  //obteniendo los elementos del DOM desde la interfaz
+  const $messageForm = $('#message-form');
+  const $messageBox = $('#message');
+  const $chat = $('#chat');
 
-    //obteniendo los elementos del DOM desde la nicknameForm
-    const $nickForm = $('#nickForm');
-    const $nickError = $('#nickError');
-    const $nickname = $('#nickname');
+  //obteniendo los elementos del DOM desde la nicknameForm
+  const $nickForm = $('#nickForm');
+  const $nickError = $('#nickError');
+  const $nickname = $('#nickname');
 
-    const $users = $('usernames');
+  const $users = $('#usernames');
 
-    $nickForm.submit(e =>{
-      e.preventDefault();
-      socket.emit('new user' , $nickname.val() , data => {
-        if (data) {
-          $('#nickWrap').hide();
-          $('#contentWrap').show();
+  $nickForm.submit(e => {
+    e.preventDefault();
+    socket.emit('new user', $nickname.val(), data => {
+      if (data) {
+        $('#nickWrap ').hide();
+        $('#contentWrap').show();
 
-        }else {
-          $nickError.html(`
+      } else {
+        $nickError.html(`
            <div class="alert alert-danger">
               ese usuario ya existe.
            </div>
           `);
-        }
-
-        $nickname.val('');
-
-      });
-
-    });
-
-    // eventos
-    $messageForm.submit( e => {
-        e.preventDefault();
-        socket.emit('send message' , $messageBox.val());
-        $messageBox.val('');
-    });
-    
-    socket.on('new message' , function (data){
-       $chat.append(data + '<br/>');
-
-
-    });
-
-    socket.on('usernames', data =>{
-      let html = '';
-      for (let i = 0; i < data.length; i++) {
-        html += `<p><i class="fas fa-user"></i>${data[i]}</p>`;
-
       }
 
-       $users.html(html);
+      $nickname.val('');
 
     });
+
+  });
+
+  // eventos
+  $messageForm.submit(e => {
+    e.preventDefault();
+    socket.emit('send message', $messageBox.val());
+    $messageBox.val('');
+  });
+
+  socket.on('new message', function (data) {
+    $chat.append(data + '<br/>');
+  });
+
+  socket.on('usernames', data => {
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+      html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`;
+    }
+    $users.html(html);
+  });
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,30 +1,30 @@
-module.exports = function (io){
+module.exports = function (io) {
 
-    let nicknames = [];
+  let nicknames = [];
 
-    io.on('connection', socket => {
-        console.log('new user connected');
+  io.on('connection', socket => {
+    console.log('new user connected');
 
-        socket.on('new user', (data, cb) => {
-          if (nicknames.indexOf(data) != -1){
-             cb(false);
-          }else {
-            cb(true);
-            socket.nickname = data;
-            nicknames.push(socket.nickname);
-            io.sockets.emit('usernames', nicknames);
+    socket.on('new user', (data, cb) => {
+      if (nicknames.indexOf(data) != -1) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        nicknames.push(socket.nickname);
+        io.sockets.emit('usernames', nicknames);
 
-          }
+      }
 
-        });
-
-        socket.on('send message' , function (data) {
-            io.sockets.emit('new message' , data); 
-        });
-
-    
-        
     });
+
+    socket.on('send message', function (data) {
+      io.sockets.emit('new message', data);
+    });
+
+
+
+  });
 
 
 }
